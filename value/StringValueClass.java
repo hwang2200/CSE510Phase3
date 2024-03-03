44d361e96b446ac6bddae6e654ef123870710b60
NathanaelWang <85484683+NathanaelWang@users.noreply.github.com>
2024-03-02 19:22:09 -0700

    Add files via upload

---
+++ value/StringValueClass.java
@@ -0,0 +1,24 @@
+class StringValueClass extends ValueClass
+{
+	private String value;
+	
+	public StringValue()
+	{
+
+	}
+	
+	public StringValueClass(String value)
+	{
+		this.value = value;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+}
