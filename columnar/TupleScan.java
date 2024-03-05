6139a8cb8e4bed293cbba332f496e47df20b195e
hwang2200 <94589723+hwang2200@users.noreply.github.com>
2024-03-05 13:53:12 -0700

    Add files via upload

---
+++ columnar/TupleScan.java
@@ -0,0 +1,30 @@
+package Project2.columnar;
+
+import Project2.TID;
+import heap.InvalidTupleSizeException;
+import heap.Tuple;
+
+public class TupleScan
+{
+    //Constructor
+    public TupleScan(ColumnarFile cf) throws InvalidTupleSizeException
+    {
+
+    }
+
+    //Methods
+    public void closetuplescan()
+    {
+        reset();
+    }
+
+    public Tuple getNext(TID tid)
+    {
+
+    }
+
+    public boolean position(TID tid)
+    {
+
+    }
+}
